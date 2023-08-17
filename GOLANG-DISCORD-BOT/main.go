62da8f8a48bf1086804d472eaf34df4557968d2c
daristylee <106734478+daristylee@users.noreply.github.com>
2023-08-18 00:49:09 +0300

    Update main.go

--- GOLANG-DISCORD-BOT/main.go
+++ GOLANG-DISCORD-BOT/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"container/list"
 	"context"
 	"encoding/json"
 	"fmt"
@@ -11,6 +12,7 @@ import (
 	"os"
 	"strconv"
 	"strings"
+	"time"
 	"unicode"
 
 	"github.com/bwmarrin/discordgo"
@@ -26,6 +28,11 @@ var (
 type configStruct struct {
 	Token     string `json:"Token"`
 	BotPrefix string `json:"BotPrefix"`
+}
+
+type Bet struct {
+	author, winner, loser string
+	win                   int64
 }
 
 func ReadConfig() error {
@@ -55,6 +62,7 @@ func ReadConfig() error {
 var BotId string
 var goBot *discordgo.Session
 var balance = make(map[string]int64)
+var bets = make(map[int64]*list.List)
 
 func Start() {
 	goBot, err := discordgo.New("Bot " + config.Token)
@@ -109,14 +117,14 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 		i++
 		for unicode.IsDigit(rune(m.Content[i])) {
-			sum = sum*10 + int(m.Content[i]-'0')
+			sum = sum*10 + int64(m.Content[i]-'0')
 			i++
 		}
 
 		balance[user] += sum
-		_, _ = s.ChannelMessageSend(m.ChannelID, strconv.Itoa(sum)+" were added to "+user+"'s balance")
+		_, _ = s.ChannelMessageSend(m.ChannelID, strconv.Itoa(int(sum))+" were added to "+user+"'s balance")
 	} else if m.Content == BotPrefix+"show" {
-		_, _ = s.ChannelMessageSend(m.ChannelID, "Your balance is "+strconv.Itoa(balance[m.Author.ID]))
+		_, _ = s.ChannelMessageSend(m.ChannelID, "Your balance is "+strconv.Itoa(int(balance[m.Author.ID])))
 	} else if strings.HasPrefix(m.Content, BotPrefix+"show ") {
 		m.Content += " "
 		var user = ""
@@ -126,7 +134,7 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 			i++
 		}
 
-		_, _ = s.ChannelMessageSend(m.ChannelID, user+"'s balance is "+strconv.Itoa(balance[user]))
+		_, _ = s.ChannelMessageSend(m.ChannelID, user+"'s balance is "+strconv.Itoa(int(balance[user])))
 	} else if m.Content == BotPrefix+"help" {
 		_, _ = s.ChannelMessageSend(m.ChannelID, "```show user: shows user's balance"+"\n"+"show: shows your balance"+
 			"\n"+"add user sum: adds sum to user's balance```")
@@ -164,13 +172,13 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 
 		_, _ = s.ChannelMessageSend(m.ChannelID, strconv.Itoa(cota1/100)+"."+strconv.Itoa(cota1/10%10)+strconv.Itoa(cota1%10)+"-"+strconv.Itoa(cota2/100)+"."+strconv.Itoa(cota2/10%10)+strconv.Itoa(cota2%10))
-	} else if strings.HasPrefix(m.Content, BotPrefix+"bet ") {
+	} else if strings.HasPrefix(m.Content, BotPrefix+"bet cf ") {
 		m.Content += " "
 		var user1 = ""
 		var user2 = ""
 		var sum int64 = 0
 		var id int64 = 0
-		var i = 5
+		var i = 8
 		for m.Content[i] != ' ' {
 			user1 += string(m.Content[i])
 			i++
@@ -182,31 +190,48 @@ func messageHandler(s *discordgo.Session, m *discordgo.MessageCreate) {
 		}
 		i++
 		for unicode.IsDigit(rune(m.Content[i])) {
-			sum = sum*10 + int(m.Content[i]-'0')
+			sum = sum*10 + int64(m.Content[i]-'0')
 			i++
 		}
 		i++
 		for unicode.IsDigit(rune(m.Content[i])) {
-			id = id*10 + int(m.Content[i]-'0')
+			id = id*10 + int64(m.Content[i]-'0')
 			i++
 		}
 
 		ctx := context.Background()
 		logger := log.New(os.Stderr, "*** ", log.LstdFlags)
 		api, _ := goforces.NewClient(logger)
-		contest := api.GetContestList()
-
-		var ratingChange1, _ = api.GetUserRating(ctx, user1)
-		var ratingChange2, _ = api.GetUserRating(ctx, user2)
-		var rating1 = ratingChange1[len(ratingChange1)-1].NewRating
-		var rating2 = ratingChange2[len(ratingChange2)-1].NewRating
-		var cota1, cota2 = cota(rating1, rating2)
-		cota1 += 1
-		if cota1 > 50100 {
-			cota1 = 50100
+		contestList, _ := api.GetContestList(ctx, nil)
+		i = 0
+		for contestList[i].ID != id && !contestList[i].Finished() {
+			i++
 		}
 
-		_, _ = s.ChannelMessageSend(m.ChannelID, strconv.Itoa(cota1/100)+"."+strconv.Itoa(cota1/10%10)+strconv.Itoa(cota1%10)+"-"+strconv.Itoa(cota2/100)+"."+strconv.Itoa(cota2/10%10)+strconv.Itoa(cota2%10))
+		if !(contestList[i].ID == id && !contestList[i].Finished()) {
+			_, _ = s.ChannelMessageSend(m.ChannelID, "The contest is invalid!")
+		} else if balance[m.Author.ID] >= sum {
+
+			balance[m.Author.ID] -= sum
+
+			var ratingChange1, _ = api.GetUserRating(ctx, user1)
+			var ratingChange2, _ = api.GetUserRating(ctx, user2)
+			var rating1 = ratingChange1[len(ratingChange1)-1].NewRating
+			var rating2 = ratingChange2[len(ratingChange2)-1].NewRating
+			var cota1, cota2 = cota(rating1, rating2)
+			cota1 += 1
+			if cota1 > 50100 {
+				cota1 = 50100
+			}
+			var win int64 = (sum - sum/20) * int64(cota1) / 100
+
+			bet := Bet{m.Author.ID, user1, user2, win}
+			bets[id].PushBack(bet)
+
+			_, _ = s.ChannelMessageSend(m.ChannelID, "You bet "+strconv.Itoa(int(sum))+" on "+user1+" vs "+user2+" in the Codforces contest: "+strconv.Itoa(int(id))+" with a potentially win of "+strconv.Itoa(int(win)))
+		} else {
+			_, _ = s.ChannelMessageSend(m.ChannelID, "Insufficient funds")
+		}
 	}
 }
 
@@ -220,7 +245,29 @@ func main() {
 	}
 
 	Start()
+	fmt.Println("ok")
+
+	const ONE_DAY = 24 * 60 * 60
+
+	ctx := context.Background()
+	logger := log.New(os.Stderr, "*** ", log.LstdFlags)
+	api, _ := goforces.NewClient(logger)
+
+	for true {
+		time.Sleep(ONE_DAY)
+		contestList, _ := api.GetContestList(ctx, nil)
+		for key, value := range bets {
+			i := 0
+			for contestList[i].ID != key {
+				i++
+			}
+			if contestList[i].Finished() {
+			
+			}
+		}
+	}
 
 	<-make(chan struct{})
 	return
 }
+
